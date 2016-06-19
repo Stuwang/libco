1a36db7656ec6d4414135d508bb77b5d4d3e6c12
wangyuan <799086345@qq.com>
2016-06-19 17:02:57 +0800

    删除多余内容

--- co_mutex.h
+++ co_mutex.h
@@ -13,15 +13,11 @@ public:
 	co_mutex(): locked_(false) {};
 	inline void lock() {
 		std::unique_lock<spinlock> lk(lock_);
-		// lock_.lock();
 		if (locked_) {
 			auto cur_task = gettaskinstense().getCurTask();
-			// gettaskinstense().cancleTask(cur_task);
 			tasklist_.put(cur_task);
 			cur_task->SetState(TaskState::Sysblock);
-			// lock_.unlock();
 			lk.unlock();
-			// std::cout << "lock it " << gettaskinstense().size() << std::endl;
 			gettaskinstense().yield();
 		} else {
 			locked_ = true;
@@ -29,16 +25,13 @@ public:
 		}
 	};
 	inline void unlock() {
-		// std::cout << "unlock it " << std::endl;
 		std::unique_lock<spinlock> lk(lock_);
 
 		if (tasklist_.size()) {
 			auto p = this->tasklist_.get();
 			p->SetState(TaskState::Runnable);
 			gettaskinstense().addTask(p);
-			// std::cout << "size: " << gettaskinstense().size() << std::endl;
 		} else {
-			// std::cout << "what the fuck! " << std::endl;
 			if (!locked_) {
 				assert(false);
 			}
--- coroute.cpp
+++ coroute.cpp
@@ -1,34 +1,34 @@
 #include "coroute.h"
 
-namespace co{
+namespace co {
 
-Coroute::Coroute(Functor f,Task * ptask)
+Coroute::Coroute(Functor f, Task * ptask)
 {
 	stack_size_ = STACK_SIZE;
 	stack_.reset( new char[STACK_SIZE] );
 	getcontext(&ctx_);
-	ctx_.uc_stack.ss_sp = static_cast<char*>(stack_.get()) ; 
+	ctx_.uc_stack.ss_sp = static_cast<char*>(stack_.get()) ;
 	ctx_.uc_stack.ss_size = stack_size_;
 	ctx_.uc_link = NULL;
-	makecontext(&ctx_,f,1,ptask );
+	makecontext(&ctx_, f, 1, ptask );
 };
 
-Coroute::~Coroute(){};
+Coroute::~Coroute() {};
 
 ucontext_t & Coroute::GetThreadLocalContext()
 {
-	static thread_local ucontext_t local; 
+	static thread_local ucontext_t local;
 	return local;
 };
 
 bool Coroute::SwapIn()
 {
-	return 0 == swapcontext(&GetThreadLocalContext(),&ctx_);
+	return 0 == swapcontext(&GetThreadLocalContext(), &ctx_);
 };
 
 bool Coroute::SwapOut()
 {
-	return 0 == swapcontext(&ctx_,&GetThreadLocalContext() );
+	return 0 == swapcontext(&ctx_, &GetThreadLocalContext() );
 };
 
 }//
